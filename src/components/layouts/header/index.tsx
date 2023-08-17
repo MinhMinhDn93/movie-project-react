acc00093bf45d955b72570f323a1aa2fa2009355
minh <minh9121993@gmail.com>
2023-08-17 11:19:50 +0700

    up

--- src/components/layouts/header/index.tsx
+++ src/components/layouts/header/index.tsx
@@ -1,5 +1,18 @@
+import styles from '../header/Header.module.css'
+import HeaderLogo from '../../HeaderLogo'
+import SearchMovie from '../../HeaderSearch/SearchMovie';
+import User from '../../User';
+
 function Header() {
-  return <div className="container mx-auto">day la Header</div>;
+  return (<header className={styles.header}>
+    <div className= {styles.container}>
+      <div className={styles.header_wrapper}>
+        <HeaderLogo />
+        <SearchMovie/>
+        <User/>
+      </div>
+    </div>
+  </header>);
 }
 
 export default Header;
